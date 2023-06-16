1a9e6a6cbdcc6c67d9878a238df296e58e286066
Abdallah Fonseca <abdallahfonsecam@student.douglascollege.ca>
2023-06-15 17:41:26 -0700

    initial commit

--- Main.java
+++ Main.java
@@ -6,36 +6,31 @@ public class Main {
 	public static void main(String[] args) {
 
 		TwoDArray();
-		//shuffleNum();
-
-
 
 	}
 
 	
 	
 	public static void TwoDArray() {
-		
+		int [][] array2D = new int[5][5];
 		Random random = new Random();
-		int[][] array2d = new int [5][5];
 		ArrayList<Integer> numbers = new ArrayList<Integer>();
-		
-		for(int i = 1; i < 26; i++) {
-			numbers.add(i);			
-		}
-	
-		
-		
-		for(int i = 0; i < array2d.length; i++) {
-			System.out.print("|");
-			for(int j = 0; j < array2d[i].length; j++) {
-				var index = random.nextInt(25-((i*5)+(j)));
-				
 
+		for(int i = 0; i<26; i++){
+
+			numbers.add(i);
+		}
+		//System.out.print(numbers);
+
+		for(int i = 0; i < array2D.length; i++){
+			for(int j = 0; j < array2D[i].length; j++){
+				var index = random.nextInt(25 - ((i*5)+(j)));
 				System.out.print(numbers.remove(index)+" ");
 			}
-			System.out.println("|");
+			System.out.println();
 		}
+
+
 
 
  
